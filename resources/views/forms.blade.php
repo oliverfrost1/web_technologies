3ac3f917bb74d53b6f83c727de1d9e0b435655bf
Oliver Frost <oliverfrost11@gmail.com>
2023-09-06 12:08:58 +0200

    Forms and gitignore

---
+++ resources/views/forms.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<HTML>
+
+<HEAD>
+    <TITLE>Example App</TITLE>
+</HEAD>
+<body>
+  <form>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" placeholder="Skriv dit navn">
+    <input type="submit" value="Submit" >
+  </form>
+</body>
+  </HTML
