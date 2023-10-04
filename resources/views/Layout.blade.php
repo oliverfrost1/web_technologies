e19e151960dc6a4fdd25e29cc1ba008615f83788
Oliver Frost <oliverfrost11@gmail.com>
2023-10-04 09:08:30 +0200

    formatting

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html> <head>
-  <title>Todo app</title>
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <script src="{{ asset('js/app.js') }}" defer></script>
+<html>
+
+<head>
+    <title>Todo app</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
-    <body>
-        @yield('content')
-    </body>
+
+<body>
+    @yield('content')
+</body>
+
 </html>
