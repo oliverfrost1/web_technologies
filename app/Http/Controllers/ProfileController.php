b04c5460a5a369230e5a2410b2ad7b541ab45e84
jlars22 <jonas.larsen@cardlay.com>
2023-10-25 18:45:43 +0200

    change password working

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -17,15 +17,16 @@ class ProfileController extends Controller
             'name' => 'required',
             'email' => 'required|email|unique:users,email,' . $user->id,
             'current_password' => 'nullable|min:8',
-            'new_password' => 'nullable|min:8',
+            'new_password' => 'nullable|same:confirm_new_password|min:8',
             'confirm_new_password' => 'nullable|same:new_password',
         ]);
 
         $dataChanged = false;
 
-        if ($request->filled('new_password')) {
-            $user->password = Hash::make($request->input('new_password'));
-            $dataChanged = true;
+        if ($request->filled('current_password') && !Hash::check($request->input('current_password'), $user->password)) {
+            return back()->withErrors([
+                'current_password' => 'Current password is incorrect',
+            ]);
         }
 
         if ($request->input('name') !== $user->name) {
@@ -38,14 +39,17 @@ class ProfileController extends Controller
             $dataChanged = true;
         }
 
-        if (!$dataChanged) {
-            return back()->with('info', 'Nothing to update');
+        if ($request->filled('new_password') && $request->filled('current_password')) {
+            $user->password = Hash::make($request->input('new_password'));
+            $dataChanged = true;
+        } else {
+            return back()->withErrors([
+                'current_password' => 'Current password is required for updating password',
+            ]);
         }
 
-        if ($request->filled('current_password') && !Hash::check($request->input('current_password'), $user->password)) {
-            return back()->withErrors([
-                'current_password' => 'Current password is incorrect',
-            ]);
+        if (!$dataChanged) {
+            return back()->with('info', 'Nothing to update');
         }
 
         $user->save();
