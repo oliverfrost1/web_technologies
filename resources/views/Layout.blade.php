3422ade797907ad20a22848bb58657b98862de5d
Oliver Frost <oliverfrost11@gmail.com>
2023-11-01 10:23:46 +0100

    Deferred JS

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
     <link rel="stylesheet" href="{{ asset('css/todo-element.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/e16fd4e2a9.js" crossorigin="anonymous"></script>
 </head>
 
@@ -14,5 +13,8 @@
     <x-top-bar />
     @yield('content')
 </body>
+<footer>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</footer>
 
 </html>
