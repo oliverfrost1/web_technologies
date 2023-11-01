9c06f57b8b89149de69356a9447262a87be419f1
jlars22 <jonas.larsen@cardlay.com>
2023-11-01 11:58:22 +0100

    Refactor ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,50 +13,84 @@ class ProfileController extends Controller
     {
         $user = Auth::user();
 
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email,' . $user->id,
-            'current_password' => 'nullable|min:8',
-            'new_password' => 'nullable|min:8',
-            'confirm_new_password' => 'nullable|same:new_password',
-        ]);
+        $validationMessage = $this->validateInput($request, $user);
 
-        $dataChanged = false;
+        $shouldUpdate = $this->shouldUpdate($request, $user);
 
-        if ($request->filled('current_password') && !Hash::check($request->input('current_password'), $user->password)) {
-            return back()->withErrors([
-                'current_password' => 'Current password is incorrect',
-            ]);
+        if ($validationMessage) {
+            return back()->withErrors($validationMessage);
         }
 
-        if ($request->input('name') !== $user->name) {
-            $user->name = $request->input('name');
-            $dataChanged = true;
-        }
-
-        if ($request->input('email') !== $user->email) {
-            $user->email = $request->input('email');
-            $dataChanged = true;
-        }
-
-        if ($request->filled('new_password') && $request->filled('current_password')) {
-            $user->password = Hash::make($request->input('new_password'));
-            $dataChanged = true;
-        } else {
-            return back()->withErrors([
-                'current_password' => 'Current password is required for updating password',
-            ]);
-        }
-
-        if (!$dataChanged) {
+        if (!$shouldUpdate) {
             return back()->with('info', 'Nothing to update');
         }
 
         $user->save();
 
         return back()->with('success', 'Profile updated successfully');
-
     }
 
+
+    private function shouldUpdate(Request $request, $user)
+    {
+        $dataChanged = false;
+
+        if ($this->isNameChanged($request, $user)) {
+            $user->name = $request->input('name');
+            $dataChanged = true;
+        }
+
+        if ($this->isEmailChanged($request, $user)) {
+            $user->email = $request->input('email');
+            $dataChanged = true;
+        }
+
+        if ($this->isPasswordChanged($request, $user)) {
+            $user->password = Hash::make($request->input('new_password'));
+            $dataChanged = true;
+        }
+
+        return $dataChanged;
+    }
+
+    private function validateInput(Request $request, $user)
+    {
+        $validator = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'current_password' => 'nullable',
+            'new_password' => 'nullable|min:8',
+            'confirm_new_password' => 'nullable|same:new_password',
+        ]);
+
+        if (!$this->isCurrentPasswordValid($request, $user)) {
+            return ['current_password' => 'Current password is incorrect'];
+        }
+
+        return null;
+    }
+
+    private function isCurrentPasswordValid(Request $request, $user)
+    {
+        if ($request->filled('current_password')) {
+            return Hash::check($request->input('current_password'), $user->password);
+        }
+    }
+
+
+    private function isNameChanged(Request $request, $user)
+    {
+        return $request->input('name') !== $user->name;
+    }
+
+    private function isEmailChanged(Request $request, $user)
+    {
+        return $request->input('email') !== $user->email;
+    }
+
+    private function isPasswordChanged(Request $request, $user)
+    {
+        return $request->filled('new_password') && $request->filled('current_password');
+    }
 
 }
