59131d24fbde9c1d7d33382f0e21105051b790d3
Oliver Frost <oliverfrost11@gmail.com>
2023-11-29 10:35:33 +0100

    Moved onClick out of main

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,4 +1,19 @@
 /**
+ * This hides the due date until "Add due date" is clicked
+ */
+const dueDate = document.getElementById("duedate");
+const dueDateButton = document.getElementById("dueDateButton");
+
+// This makes the due date selector visible
+function showDueDate() {
+    dueDate.type = "date";
+    dueDateButton.style.display = "none";
+}
+
+dueDateButton.addEventListener("click", showDueDate);
+
+
+/**
  * This code checks if the main elements have wrapped, and if they have, it adds justify-content: center
  */
 const todoPageLayout = document.querySelector(".todo-page-layout");
@@ -24,3 +39,13 @@ const checkWrapAndJustify = () => {
 
 window.addEventListener("resize", checkWrapAndJustify);
 checkWrapAndJustify(); // Initial check
+
+
+/**
+ * This handles the "Add todo" icon button
+ */
+
+const iconElement = document.getElementById("plus-icon-add-todo");
+iconElement.addEventListener("click", () => {
+    document.getElementById('addItemToTodo').submit()
+});
