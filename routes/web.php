98cab8f282f1f93df4949e4c4d36821d4223a65f
jlars22 <jonas.larsen@cardlay.com>
2023-12-04 19:16:57 +0100

    style: Apply Duster

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 // Authentication routes
 Route::get('auth/register', [RegisterController::class, 'showRegistrationForm'])->name('register.form');
 Route::post('auth/register', [RegisterController::class, 'register'])->name('register');
-Route::get('auth/login', [AuthController::class , 'show'])->name('login');
+Route::get('auth/login', [AuthController::class, 'show'])->name('login');
 Route::post('auth/login', [AuthController::class, 'authenticate'])->name('login');
 Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
 
@@ -27,16 +27,16 @@ Route::middleware('admin')->group(function () {
 });
 
 // Todo routes
-Route::get('/', [TodoController::class , 'showTodoList'])->name('main');
-Route::get('todos/visibility', [TodoController::class , 'changeSort'])->name('filter-todos');
-Route::post('todos', [TodoController::class , 'createTodo'])->name('createTodoElement');
+Route::get('/', [TodoController::class, 'showTodoList'])->name('main');
+Route::get('todos/visibility', [TodoController::class, 'changeSort'])->name('filter-todos');
+Route::post('todos', [TodoController::class, 'createTodo'])->name('createTodoElement');
 Route::put('todos/{id}/completion', [TodoController::class, 'changeCompletionStatus'])->name('changeCompletionStatus');
 Route::delete('todos/{id}', [TodoController::class, 'deleteTodoElement'])->name('deleteTodoElement');
 Route::put('todos/{id}', [TodoController::class, 'updateTodoFields'])->name('updateTodoFields');
 
 // Tag routes
-Route::post('todos/{id}/tags', [TodoController::class , 'addNewTagToTodo'])->name('addNewTag');
-Route::delete('todos/{todoId}/tags/{tagId}', [TodoController::class , 'removeTagAssociation'])->name('removeTagFromTodo');
-Route::delete('tags/{id}', [TodoController::class , 'removeTag'])->name('removeTag');
-Route::put('tags/{id}', [TodoController::class , 'updateTag'])->name('updateTag');
-Route::get('tags/filter', [TodoController::class , 'changeSelectedTags'])->name('filterByTags');
+Route::post('todos/{id}/tags', [TodoController::class, 'addNewTagToTodo'])->name('addNewTag');
+Route::delete('todos/{todoId}/tags/{tagId}', [TodoController::class, 'removeTagAssociation'])->name('removeTagFromTodo');
+Route::delete('tags/{id}', [TodoController::class, 'removeTag'])->name('removeTag');
+Route::put('tags/{id}', [TodoController::class, 'updateTag'])->name('updateTag');
+Route::get('tags/filter', [TodoController::class, 'changeSelectedTags'])->name('filterByTags');
