cdc82c96ef1a0b9972b5cfae56bff768dd904d9f
Oliver Frost <oliverfrost11@gmail.com>
2023-12-07 19:30:35 +0100

    Simplified TodoElement

--- resources/react/components/TodoElement.tsx
+++ resources/react/components/TodoElement.tsx
@@ -1,34 +1,13 @@
 import React from "react";
 import { Todo } from "../types/todoTypes";
-import {
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    CardHeader,
-    Typography,
-    useTheme,
-} from "@mui/material";
+import { Card, CardContent, Typography, useTheme } from "@mui/material";
 
 type TodoElementProps = {
     todo: Todo;
 };
 
-/*
-export interface Todo {
-    completed: number;
-    id: number;
-    created_at: string;
-    description: string | null;
-    title: string;
-    updated_at: string;
-    user_id: number;
-    status: TodoStatus;
-}*/
-
-export default function TodoElement({ todo }: TodoElementProps) {
+export default function TodoElement({ todo }: Readonly<TodoElementProps>) {
     const theme = useTheme();
-    // Show all elements of the todo object
     return (
         <Card
             sx={{
@@ -37,21 +16,14 @@ export default function TodoElement({ todo }: TodoElementProps) {
                 color: theme.palette.secondary.contrastText,
             }}
         >
-            <CardHeader
-                title={todo.title}
-                subheader={todo.due_date ? `Due by: ${todo.due_date}` : ""}
-            />
             <CardContent>
+                <Typography variant="h6" color="text.secondary">
+                    {todo.title}
+                </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    {todo.description}
+                    {todo.due_date ? `Due by: ${todo.due_date}` : ""}
                 </Typography>
             </CardContent>
-            <CardActions>
-                {/* TODO: Add edit button */}
-                <Button size="small" color="primary">
-                    Edit
-                </Button>
-            </CardActions>
         </Card>
     );
 }
